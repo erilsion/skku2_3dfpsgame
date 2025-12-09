52c663c55f964b66f7c769553142c8f8412afe28
erilsion <erilsion@gmail.com>
2025-12-09 15:16:35 +0900

    Fix: 체력바와 스태미너바 스크립트 내용을 잘못 적은 것 수정

--- Assets/02.Scripts/Camera/UI/UI_PlayerStats.cs
+++ Assets/02.Scripts/Camera/UI/UI_PlayerStats.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_PlayerStats : MonoBehaviour
 {
     [SerializeField] private PlayerStats _stats;
-    [SerializeField] private SliderJoint2D _healthSlider;
-    [SerializeField] private SliderJoint2D _staminaSlider;
+    [SerializeField] private Slider _healthSlider;
+    [SerializeField] private Slider _staminaSlider;
 
 
     void Update()
     {
-        //_healthSlider.value = _stats.Health.Value / _stats.Health.MaxValue;
-        //_staminaSlider.value = _stats.Stamina.Value / _stats.Stamina.MaxValue;
+        _healthSlider.value = _stats.Health.Value / _stats.Health.MaxValue;
+        _staminaSlider.value = _stats.Stamina.Value / _stats.Stamina.MaxValue;
     }
 }
