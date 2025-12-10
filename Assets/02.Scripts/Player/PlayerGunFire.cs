376e173f92e60d63264a733f5082341b80bce68a
erilsion <erilsion@gmail.com>
2025-12-10 10:54:27 +0900

    Feat: 총알 이펙트 생성

--- Assets/02.Scripts/Player/PlayerGunFire.cs
+++ Assets/02.Scripts/Player/PlayerGunFire.cs
@@ -5,12 +5,12 @@ public class PlayerGunFire : MonoBehaviour
     // 목표: 마우스 왼쪽 버튼을 누르면 카메라(플레이어)가 바라보는 방향으로 총알을 발사하고 싶다. (총알을 날리고 싶다.)
 
     [SerializeField] private Transform _fireTransform;
-    [SerializeField] private ParticleSystem _hitEffectPrefab;
+    [SerializeField] private ParticleSystem _hitEffect;
 
     private void Update()
     {
         // 1. 마우스 왼쪽 버튼이 눌린다면
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
             // 2. Ray를 생성하고 발사할 위치, 방향, 거리를 설정한다. (쏜다.)
             Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);
@@ -23,9 +23,20 @@ public class PlayerGunFire : MonoBehaviour
             if (isHit)
             {
                 Debug.Log(hitInfo.transform.name);
-                ParticleSystem hitEffect = Instantiate(_hitEffectPrefab, hitInfo.point, Quaternion.identity);
-                hitEffect.transform.forward = hitInfo.normal;
-                hitEffect.Play();
+
+                // 파티클 생성과 플레이 방식
+                // 1. Instantiate 방식 (+ 풀링) -> 한 화면에 여러가지 수정 후 여러 개 그릴 경우. 새로 생성 (메모리, CPU)
+                // 2. 하나를 캐싱해두고 Play    -> 한 화면에 한번만 그릴 경우. 단점: 재실행이므로 기존 게 삭제
+                // 3. 하나를 캐싱해두고 Emit    -> 한 화면에 위치만 수정 후 여러 개 그릴 경우
+
+                //_hitEffect.transform.position = hitInfo.point;
+                //_hitEffect.transform.forward = hitInfo.normal;
+
+                ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+                emitParams.position = hitInfo.point;
+                emitParams.rotation3D = Quaternion.LookRotation(hitInfo.normal).eulerAngles;
+
+                _hitEffect.Emit(emitParams, 1);   // 커스텀할 정보, 분출 횟수
             }
         }
     }
