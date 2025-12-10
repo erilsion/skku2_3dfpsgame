a948df1bbc6973acb7d817bcd8c6c9644ee8fec1
erilsion <erilsion@gmail.com>
2025-12-10 16:31:55 +0900

    Feat: 1210 코루틴 수업

--- Assets/02.Scripts/Camera/CameraRecoil.cs
+++ Assets/02.Scripts/Camera/CameraRecoil.cs
@@ -5,12 +5,12 @@ public class CameraRecoil : MonoBehaviour
     public static CameraRecoil Instance;
 
     [Header("반동 설정")]
-    [SerializeField] private float recoilAmount = 6f;
-    [SerializeField] private float recoilSpeed = 18f;
-    [SerializeField] private float returnSpeed = 12f;
+    [SerializeField] private float _recoilAmount = 6f;
+    [SerializeField] private float _recoilSpeed = 18f;
+    [SerializeField] private float _returnSpeed = 12f;
 
-    private Vector3 currentRecoil;
-    private Vector3 targetRecoil;
+    private Vector3 _currentRecoil;
+    private Vector3 _targetRecoil;
 
     private void Awake()
     {
@@ -19,15 +19,15 @@ public class CameraRecoil : MonoBehaviour
 
     private void Update()
     {
-        targetRecoil = Vector3.Lerp(targetRecoil, Vector3.zero, returnSpeed * Time.deltaTime);
-        currentRecoil = Vector3.Lerp(currentRecoil, targetRecoil, recoilSpeed * Time.deltaTime);
+        _targetRecoil = Vector3.Lerp(_targetRecoil, Vector3.zero, _returnSpeed * Time.deltaTime);
+        _currentRecoil = Vector3.Lerp(_currentRecoil, _targetRecoil, _recoilSpeed * Time.deltaTime);
 
-        transform.localRotation = Quaternion.Euler(currentRecoil);
+        transform.localRotation = Quaternion.Euler(_currentRecoil);
     }
 
     public void DoRecoil()
     {
         float randomX = Random.Range(-1f, 1f);
-        targetRecoil += new Vector3(-recoilAmount, randomX * recoilAmount * 0.5f, 0f);
+        _targetRecoil += new Vector3(-_recoilAmount, randomX * _recoilAmount * 0.5f, 0f);
     }
 }
---
+++ Assets/02.Scripts/Grammer/CoroutineExample2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineExample2 : MonoBehaviour
+{
+    private Coroutine _coroutine;  // 코루틴 핸들러
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);  // 현재 실행중인 코루틴 중지
+                StopAllCoroutines();        // 현재 실행중인 모든 코루틴 중지 (특별한 의도가 없으면 사용 X)
+            }
+            
+            _coroutine = StartCoroutine(Reload_Coroutine());
+        }
+    }
+
+    private IEnumerator Reload_Coroutine()
+    {
+        // 실행 제어 관점에서의 코루틴
+        // - 특정 프레임, 시간 단위로 실행을 제어하거나
+        // - 특정 조건이 만족할 때까지 실행을 제어할 수 있다.
+
+        yield return new WaitForSeconds(0.6f);
+
+        Debug.Log("총알이 재장전됐습니다.");
+    }
+}
